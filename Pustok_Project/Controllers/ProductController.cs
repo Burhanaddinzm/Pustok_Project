cedccf626c5d01aa77cc4b693cc40c94ea62fc04
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-04-11 18:26:44 +0400

    Working on product detail

---
+++ Pustok_Project/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok_Project.Data.Contexts;
+
+namespace Pustok_Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            var product = await _context.Products
+                                .Include(x => x.Category)
+                                .Include(x => x.Brand)
+                                .Include(x => x.ProductImages)
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null) return RedirectToAction("ErrorNotFound", "Home");
+            return View(product);
+        }
+    }
+}
